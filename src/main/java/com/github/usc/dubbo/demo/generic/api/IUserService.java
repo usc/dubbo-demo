250ee6be4c093e51988b7e759deddaed00b14f58
usc <lishunli.me@gmail.com>
2014-08-15 15:25:43 +0800

    fix missing changed after rename

--- src/main/java/com/github/usc/dubbo/demo/generic/api/IUserService.java
+++ src/main/java/com/github/usc/dubbo/demo/generic/api/IUserService.java
@@ -25,23 +25,25 @@ import com.github.usc.dubbo.demo.generic.api.IUserService.User;
  *
  */
 public interface IUserService extends IService<Params, User> {
-
-
-    public static class Params implements Serializable{
+    public static class Params implements Serializable {
         private static final long serialVersionUID = 1L;
 
         public Params(String query) {
         }
     }
-    public static class User implements Serializable{
+
+    public static class User implements Serializable {
         private static final long serialVersionUID = 1L;
+
         public User(int id, String name) {
             super();
             this.id = id;
             this.name = name;
         }
+
         private int id;
         private String name;
+
         public int getId() {
             return id;
         }
